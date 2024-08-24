f5872e8f7debf212d6af26d89c4b231e07d2aa80
ForeX03 <51081985+ForeX03@users.noreply.github.com>
2024-08-24 23:13:18 +0200

    Initialize command-2.x

---
+++ bukkit/src/main/java/cc/dreamcode/template/BukkitTemplatePlugin.java
@@ -0,0 +1,102 @@
+package cc.dreamcode.template;
+
+import cc.dreamcode.command.bukkit.BukkitCommand;
+import cc.dreamcode.menu.bukkit.BukkitMenuProvider;
+import cc.dreamcode.menu.bukkit.okaeri.MenuBuilderSerdes;
+import cc.dreamcode.notice.minecraft.bukkit.serdes.BukkitNoticeSerdes;
+import cc.dreamcode.platform.DreamVersion;
+import cc.dreamcode.platform.bukkit.DreamBukkitConfig;
+import cc.dreamcode.platform.bukkit.DreamBukkitPlatform;
+import cc.dreamcode.platform.bukkit.component.ConfigurationComponentResolver;
+import cc.dreamcode.platform.bukkit.component.ListenerComponentResolver;
+import cc.dreamcode.platform.bukkit.component.RunnableComponentResolver;
+import cc.dreamcode.platform.component.ComponentManager;
+import cc.dreamcode.platform.component.resolver.CommandBindComponentResolver;
+import cc.dreamcode.platform.component.resolver.CommandComponentResolver;
+import cc.dreamcode.platform.component.resolver.CommandExtensionComponentResolver;
+import cc.dreamcode.platform.component.resolver.CommandHandlerComponentResolver;
+import cc.dreamcode.platform.persistence.DreamPersistence;
+import cc.dreamcode.platform.persistence.component.DocumentPersistenceComponentResolver;
+import cc.dreamcode.platform.persistence.component.DocumentRepositoryComponentResolver;
+import cc.dreamcode.template.command.ExampleCommand;
+import cc.dreamcode.template.config.MessageConfig;
+import cc.dreamcode.template.config.PluginConfig;
+import cc.dreamcode.template.controller.ExampleUserController;
+import cc.dreamcode.template.mcversion.VersionProvider;
+import cc.dreamcode.template.user.UserRepository;
+import eu.okaeri.configs.serdes.OkaeriSerdesPack;
+import eu.okaeri.configs.yaml.bukkit.serdes.SerdesBukkit;
+import eu.okaeri.persistence.document.DocumentPersistence;
+import eu.okaeri.tasker.bukkit.BukkitTasker;
+import lombok.Getter;
+import lombok.NonNull;
+
+public final class BukkitTemplatePlugin extends DreamBukkitPlatform implements DreamBukkitConfig, DreamPersistence {
+
+    @Getter private static BukkitTemplatePlugin bukkitTemplatePlugin;
+
+    @Override
+    public void load(@NonNull ComponentManager componentManager) {
+        bukkitTemplatePlugin = this;
+    }
+
+    @Override
+    public void enable(@NonNull ComponentManager componentManager) {
+        this.registerInjectable(VersionProvider.getVersionAccessor());
+        this.registerInjectable(BukkitTasker.newPool(this));
+        this.registerInjectable(BukkitMenuProvider.create(this));
+        this.registerInjectable(BukkitCommand.create(this));
+
+        componentManager.registerResolver(CommandComponentResolver.class);
+        componentManager.registerResolver(CommandBindComponentResolver.class);
+        componentManager.registerResolver(CommandHandlerComponentResolver.class);
+        componentManager.registerResolver(CommandExtensionComponentResolver.class);
+
+        componentManager.registerResolver(ListenerComponentResolver.class);
+        componentManager.registerResolver(RunnableComponentResolver.class);
+
+        componentManager.registerResolver(ConfigurationComponentResolver.class);
+        componentManager.registerComponent(MessageConfig.class);
+        componentManager.registerComponent(PluginConfig.class, pluginConfig -> {
+            componentManager.setDebug(pluginConfig.debug);
+
+            // register persistence + repositories
+            this.registerInjectable(pluginConfig.storageConfig);
+
+            componentManager.registerResolver(DocumentPersistenceComponentResolver.class);
+            componentManager.registerResolver(DocumentRepositoryComponentResolver.class);
+
+            componentManager.registerComponent(DocumentPersistence.class);
+            componentManager.registerComponent(UserRepository.class);
+        });
+
+        componentManager.registerComponent(ExampleCommand.class);
+        componentManager.registerComponent(ExampleUserController.class);
+    }
+
+    @Override
+    public void disable() {
+        // features need to be call when server is stopping
+    }
+
+    @Override
+    public @NonNull DreamVersion getDreamVersion() {
+        return DreamVersion.create("Dream-Template", "1.0-InDEV", "author");
+    }
+
+    @Override
+    public @NonNull OkaeriSerdesPack getConfigSerdesPack() {
+        return registry -> {
+            registry.register(new BukkitNoticeSerdes());
+            registry.register(new MenuBuilderSerdes());
+        };
+    }
+
+    @Override
+    public @NonNull OkaeriSerdesPack getPersistenceSerdesPack() {
+        return registry -> {
+            registry.register(new SerdesBukkit());
+        };
+    }
+
+}
